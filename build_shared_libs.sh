124db3008f6bf66ac36ea1278d5f44212104ddef
philo <feilong.he@intel.com>
2017-08-29 11:26:11 +0800

    modify build_shared_libs.sh

--- build_shared_libs.sh
+++ build_shared_libs.sh
@@ -4,19 +4,21 @@ buildpath=$rootpath/components/src/github.com/pingcap
 cd $buildpath/pd
 echo "building pd.."
 make
-cp libpd.so $rootpath/launch
+mv libpd.so $rootpath/launch
+rm libpd.h
 make clean
 
 cd $buildpath/callkv
 echo "building tikv.."
 cargo build
-cp target/debug/libtikv.so $rootpath/launch
+mv target/debug/libtikv.so $rootpath/launch
 cargo clean
 
 cd $buildpath/tidb
 echo "building tidb.."
 export GOPATH=$rootpath/components
 make
-cp libtidb.so $rootpath/launch
+mv libtidb.so $rootpath/launch
+rm libtidb.h 
 make clean
 
